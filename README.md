4792f913ed3f88750f11adfc2302ec12db5396ac
anuj.kumar <anuj.kumar@inmobi.com>
2020-05-02 13:15:55 +0530

    Init template from https://github.com/golang-standards/project-layout

---
+++ README.md
@@ -0,0 +1,4 @@
+### TODO:
+- rpc errors
+- makefile
+- 
---
+++ pkg/core/error_helpers.go
@@ -0,0 +1,36 @@
+package core
+
+import dstk "github.com/anujga/dstk/api/protobuf-spec"
+
+const (
+	//note: for backward compatibility, we can only add entries in the end
+	ErrUnknown               = -1
+	ErrKeyNotFound           = 1
+	ErrChannelFull           = iota
+	ErrCantCreateSnapshotDir = iota
+	ErrPeerAlreadyExists     = iota
+	ErrNodeAlreadyExists     = iota
+
+	//...
+	ErrMaxErrorCode = iota
+)
+
+type Errr struct {
+	dstk.Ex
+}
+
+func NewErr(id int64, msg string) *Errr {
+	return &Errr{dstk.Ex{Id: id, Msg: msg}}
+}
+
+func FromErr(err error) *Errr {
+	return NewErr(ErrUnknown, err.Error())
+}
+
+func FromRErr(err dstk.Ex) *Errr {
+	return &Errr{err}
+}
+
+func (m *Errr) Error() string {
+	return m.Msg
+}
