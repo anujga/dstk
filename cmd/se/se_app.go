87ed360fb5fe07c8b04cd3e625979786cc6f4f7f
anujga <anujgarg2004@gmail.com>
2020-06-12 18:35:35 +0530

    SE Server app (#27)
    More build breaks

---
+++ cmd/se/se_app.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"flag"
+	"github.com/anujga/dstk/pkg/sharding_engine/simple"
+)
+
+func main() {
+	var port = flag.Int("port", 6001, "grpc port")
+	var confPath = flag.String("port", "./conf", "path of the config folder")
+	flag.Parse()
+
+	f, err := simple.StartServer(*port, *confPath)
+	if err != nil {
+		panic(err)
+	}
+
+	err = f.Wait()
+	if err != nil {
+		panic(err)
+	}
+
+}
