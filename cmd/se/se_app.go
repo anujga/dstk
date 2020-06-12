15b8cee4e18d18462e53315d6ccff7da50c83a4d
anuj <anuj.kumar@inmobi.com>
2020-06-12 22:55:54 +0530

    task file

--- cmd/se/se_app.go
+++ cmd/se/se_app.go
@@ -3,12 +3,22 @@ package main
 import (
 	"flag"
 	"github.com/anujga/dstk/pkg/sharding_engine/simple"
+	"go.uber.org/zap"
 )
 
 func main() {
 	var port = flag.Int("port", 6001, "grpc port")
 	var confPath = flag.String("conf", "./conf", "path of the config folder")
 	flag.Parse()
+
+	c := zap.NewDevelopmentConfig()
+	c.Level = zap.NewAtomicLevelAt(zap.InfoLevel)
+	log, err := c.Build()
+	if err != nil {
+		panic(err)
+	}
+
+	zap.ReplaceGlobals(log)
 
 	f, err := simple.StartServer(*port, *confPath)
 	if err != nil {
