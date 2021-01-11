d015a17a0512ae6d0f8a3798275b2f286c13dba9
anuj <anuj.kumar@inmobi.com>
2021-01-11 23:25:32 +0530

    k8s works without pvc

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -7,5 +7,5 @@ docker build -f $1 . -t "$IMAGE"
 docker tag "$IMAGE" "localhost:5000/dstk:latest"
 docker push "localhost:5000/dstk:latest"
 
-docker tag "$IMAGE" "004245605591.dkr.ecr.ap-south-1.amazonaws.com/dstk:latest"
-docker push "004245605591.dkr.ecr.ap-south-1.amazonaws.com/dstk:latest"
+#docker tag "$IMAGE" "004245605591.dkr.ecr.ap-south-1.amazonaws.com/dstk:latest"
+#docker push "004245605591.dkr.ecr.ap-south-1.amazonaws.com/dstk:latest"
